d729c2325ff72e2dbe5d01e42baa0f5326826230
hd-an <18532515775@163.com>
2024-03-08 18:06:13 +0800

    修改constant

--- packages/ui/src/store/constant.js
+++ packages/ui/src/store/constant.js
@@ -6,13 +6,18 @@ export const appDrawerWidth = 320
 export const maxScroll = 100000
 
 const COOKIE_LOWCODER_CE_SELFHOST_TOKEN_KEY = 'Flowise'
-// export const baseURL = 'http://localhost:3100'
+// export const baseURL = 'http://localhost:3000'
 export const baseURL = process.env.NODE_ENV === 'production' ? window.location.origin : window.location.origin.replace(':8080', ':3000')
-export const LOW_CODER_CREATED_BY = (cookie.parse(document.cookie)[COOKIE_LOWCODER_CE_SELFHOST_TOKEN_KEY] || '').split(':')[2]
+export const LOW_CODER_CREATED_BY =
+    process.env.NODE_ENV === 'production'
+        ? (cookie.parse(document.cookie)[COOKIE_LOWCODER_CE_SELFHOST_TOKEN_KEY] || '').split(':')[2]
+        : '65536f412886657aeb845dc9'
 // export const createdBy = '65536f412886657aeb845dc9'
 // export const orgId = '65536f422886657aeb845dca'
-export const LOW_CODER_ORG_ID = (cookie.parse(document.cookie)[COOKIE_LOWCODER_CE_SELFHOST_TOKEN_KEY] || '').split(':')[4]
-
+export const LOW_CODER_ORG_ID =
+    process.env.NODE_ENV === 'production'
+        ? (cookie.parse(document.cookie)[COOKIE_LOWCODER_CE_SELFHOST_TOKEN_KEY] || '').split(':')[4]
+        : '65536f422886657aeb845dca'
 export const uiBaseURL = window.location.origin
 export const FLOWISE_CREDENTIAL_ID = 'FLOWISE_CREDENTIAL_ID'
 export const REDACTED_CREDENTIAL_VALUE = '_FLOWISE_BLANK_07167752-1a71-43b1-bf8f-4f32252165db'
