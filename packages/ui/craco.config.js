311a4834de1f6f85c2a4c162caad16300cfac495
Joe <1712833832@qq.com>
2024-03-22 11:49:05 +0800

    craco 改为原来的配置

--- packages/ui/craco.config.js
+++ packages/ui/craco.config.js
@@ -3,31 +3,17 @@ const dotenv = require('dotenv')
 dotenv.config()
 module.exports = {
     webpack: {
-        configure: (webpackConfig, { env, paths }) => {
-            // 合并您的现有配置
-            webpackConfig.module.rules.push({
-                test: /\.m?js$/,
-                resolve: {
-                    fullySpecified: false
-                }
-            })
-
-            // 添加新的 HTML 页面路径配置
-            if (env === 'production') {
-                webpackConfig.entry = {
-                    index: [
-                        paths.appIndexJs
-                        // 添加其他原有入口文件路径
-                    ],
-                    ai_chat: './public/ai_chat.html' // 新 HTML 页面路径
-                }
-
-                webpackConfig.output = {
-                    ...webpackConfig.output
-                }
+        configure: {
+            module: {
+                rules: [
+                    {
+                        test: /\.m?js$/,
+                        resolve: {
+                            fullySpecified: false
+                        }
+                    }
+                ]
             }
-
-            return webpackConfig
         }
     }
 }
